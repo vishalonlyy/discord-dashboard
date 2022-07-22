4df0622727410cbbd23644b052a3bf7d1de236f2
vishal889 <vishalrajpurohit416@gmail.com>
2022-07-22 18:15:28 +0000

    first commit

---
+++ Src/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {page:'Home', menuId:'home'});
+});
+
+module.exports = router;
