7e45bfedb164f34ce7aef737bbb92705ca9ec06b
vishal889 <vishalrajpurohit416@gmail.com>
2022-07-25 08:49:00 +0000

    New:Updates

--- Src/app.js
+++ Src/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var teamRouter = require('./routes/developer');
 var usersRouter = require('./routes/users');
 var index = require('./routes/index');
 var app = express();
@@ -24,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/team', teamRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
